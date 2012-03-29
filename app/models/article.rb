8b5b659aa372c84f53e76626ad9054174ab6eb8b
v.morhachov <v.morhachov@vbr-bank.com.ua>
2012-03-29 14:31:12 +0300

    Before refactoring

--- app/models/article.rb
+++
@@ -1,5 +0,0 @@
-class Article < ActiveRecord::Base
-  belongs_to :action
-  belongs_to :factor
-  belongs_to :select_type
-end
--- app/models/select_type.rb
+++
@@ -1,3 +0,0 @@
-class SelectType < ActiveRecord::Base
-  has_many :articles
-end
